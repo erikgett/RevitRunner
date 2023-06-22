4d867e674fd13543351678959341d68306a252e0
erikgett <erikgette63@gmail.com>
2023-06-22 20:47:35 +0500

    Сделал основу для работы с журналами ревит

---
+++ RevitRunner/BaseRevitProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace RevitRunner
+{
+    /// <summary>
+    ///  Базовый пример запуска ревита, для этого необходимо указать путь к файлу ревита, а так же исполняемый журнал
+    ///  В ревит подгружаются только те плагины, которые расположены рядом с файлом журнала ( Подгрузятся только те addin-ы, которые в той же папке что и журнал )
+    /// </summary>
+    public class BaseRevitProcess
+    {
+        Process revitProcess = new Process();
+        /// <summary> Запускает экземляр ревита, который выполняет действия из журнала </summary>
+        /// <param name="revitPath">Путь к файл ревита, для его дальнейшего запуска</param>
+        /// <param name="argument">Журнал ревита является аргументом запуска процесса Revit</param>
+        public void ProccesExecute(string revitPath = @"C:\Program Files\Autodesk\Revit 2021\Revit.exe", string argument = @"C:\Users\gette.e\AppData\Roaming\Autodesk\Revit\Addins\2021\journall.0001.txt")
+        {
+            try
+            {                               
+                revitProcess.StartInfo.FileName = revitPath;
+                revitProcess.StartInfo.Arguments = argument;
+                // Запускаем процесс
+                revitProcess.Start();
+            }
+            catch { }
+        }
+
+        /// <summary> Убивает экземляр ревита, который был запущен </summary>
+        public void ProccesKill() { revitProcess.Kill(); }
+    }
+}
---
+++ journale_cleaner/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace journale_cleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Оставил перемынные такими для своего комфорта далее заменяются консольным вводом
+            string filePath = @"C:\Users\gette.e\AppData\Roaming\Autodesk\Revit\Addins\2021\journal.0004.txt"; // Замените на фактический путь к вашему файлу
+            string outPath = @"C:\Users\gette.e\AppData\Roaming\Autodesk\Revit\Addins\2021\journall.0004.txt";
+
+            Console.WriteLine("Введите путь к журналу");
+            filePath = Console.ReadLine();
+            outPath = Path.GetFileName(filePath);
+
+            try
+            {
+                Encoding win1251 = Encoding.GetEncoding("windows-1251");
+                string[] lines = File.ReadAllLines(filePath, win1251);
+
+                // Создаем регулярное выражение для поиска строк, начинающихся с символа ' или этот символ первый после пробелов
+                Regex regex = new Regex(@"^\s*'");
+
+                // Отфильтровываем строки и сохраняем только те, которые не соответствуют регулярному выражению
+                lines = Array.FindAll(lines, line => !regex.IsMatch(line));
+                List<string> modifiedLines = new List<string>(lines);
+                modifiedLines.Insert(0, "'");
+                // Записываем изменения обратно в файл
+                File.WriteAllLines(outPath, modifiedLines, win1251);
+            }
+            catch { }
+        }
+    }
+}
